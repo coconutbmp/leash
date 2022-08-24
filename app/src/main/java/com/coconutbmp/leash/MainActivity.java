a1b5f1387fd1ec2f1b83ede499d0b2566d6050dc
Liam Carstens <lordinfernus09@gmail.com>
2022-08-24 21:49:04 +0200

    Added Sign Up Card

--- app/src/main/java/com/coconutbmp/leash/MainActivity.java
+++ app/src/main/java/com/coconutbmp/leash/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.cardview.widget.CardView;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +17,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button showSignIn = findViewById(R.id.btnShowSignIn);
+        Button showSignUp = findViewById(R.id.btnShowSignUp);
+        ImageView closeSignIn = findViewById(R.id.imgCloseSignIn);
+        ImageView closeSignUp = findViewById(R.id.imgCloseSignUp);
+        TextView signUpFromSignIn = findViewById(R.id.lblOpenSignUp);
         CardView signInCard = findViewById(R.id.SignInCard);
+        CardView signUpCard = findViewById(R.id.SignUpCard);
 
         showSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                signUpCard.setVisibility(View.GONE);
                 signInCard.setVisibility(View.VISIBLE);
+            }
+        });
+
+        showSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInCard.setVisibility(View.GONE);
+                signUpCard.setVisibility(View.VISIBLE);
+            }
+        });
+
+        closeSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInCard.setVisibility(View.GONE);
+            }
+        });
+
+        closeSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUpCard.setVisibility(View.GONE);
+            }
+        });
+
+        signUpFromSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInCard.setVisibility(View.GONE);
+                signUpCard.setVisibility(View.VISIBLE);
             }
         });
     }
