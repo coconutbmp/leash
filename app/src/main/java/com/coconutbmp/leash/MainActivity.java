b5cc3ffea5f24c003120d0d3cf0284f0ca03591f
Liam Carstens <lordinfernus09@gmail.com>
2022-08-29 14:34:55 +0200

    Started Sign In validation

--- app/src/main/java/com/coconutbmp/leash/MainActivity.java
+++ app/src/main/java/com/coconutbmp/leash/MainActivity.java
@@ -4,22 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences prefs;
+    boolean validLogIn, validSignUp;
     Button openSignIn, openSignUp;
     ImageView closeSignIn, closeSignUp;
     TextView moveSignIn, moveSignUp;
     CardView SignInCard, SignUpCard, googleLogIn, facebookLogIn;
     CheckBox StaySignedIn;
+    EditText logInEmail, logInPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         googleLogIn = findViewById(R.id.googleCard);
         facebookLogIn = findViewById(R.id.facebookCard);
         StaySignedIn = findViewById(R.id.cbxStaySignedIn);
+        logInEmail = findViewById(R.id.edtSignInEmail);
+        logInPass = findViewById(R.id.edtSignInPass);
 
         StaySignedIn.setChecked(prefs.getBoolean("StaySignedIn", false));
 
@@ -118,10 +129,89 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 SharedPreferences.Editor editor = prefs.edit();
-                if(StaySignedIn.isChecked() == true){
+                if(StaySignedIn.isChecked() == true) {
                     editor.putBoolean("StaySignedIn", true);
+                }
+                else{
+                    editor.putBoolean("StaySignedIn", false);
+                }
+                editor.commit();
+            }
+        });
+
+        logInEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!validateEmail()){
+                    logInEmail.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 50), PorterDuff.Mode.SRC_ATOP);
+                }
+                else{
+                    logInEmail.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
                 }
             }
         });
+
+        logInPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!validatePass()){
+                    logInPass.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 50), PorterDuff.Mode.SRC_ATOP);
+                }
+                else{
+                    logInPass.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
+                }
+            }
+        });
+    }
+
+    boolean validateEmail(){
+        String email = logInEmail.getText().toString();
+        if(!email.contains("@") || !email.contains(".")){
+            return false;
+        }
+        return true;
+    }
+
+    boolean validatePass(){
+        String pass = logInPass.getText().toString();
+        boolean capitalFlag = false, numFlag = false, lowerFlag = false;
+        if(pass.length() < 8){
+            return false;
+        }
+        for (int i = 0; i < pass.length(); i++){
+            char c = pass.charAt(i);
+            if(Character.isUpperCase(c)){
+                capitalFlag = true;
+            }
+            else if(Character.isDigit(c)){
+                numFlag = true;
+            }
+            else if(Character.isLowerCase(c)){
+                lowerFlag = true;
+            }
+        }
+        if(!capitalFlag || !lowerFlag || !numFlag){
+            return false;
+        }
+        return true;
     }
 }
