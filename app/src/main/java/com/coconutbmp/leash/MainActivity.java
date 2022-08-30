977f3606738e5b3a72b6df79a7cce10e5c4eb777
Liam Carstens <lordinfernus09@gmail.com>
2022-08-30 15:40:20 +0200

    Finished Most Sign Stuff except Facebook and database things

--- app/src/main/java/com/coconutbmp/leash/MainActivity.java
+++ app/src/main/java/com/coconutbmp/leash/MainActivity.java
@@ -13,18 +13,14 @@ import androidx.cardview.widget.CardView;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Base64;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -42,25 +38,22 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Set;
+import java.util.Random;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private static final String EMAIL = "email";
     SharedPreferences prefs;
-    boolean validLogIn, validSignUp;
-    Button openSignIn, openSignUp;
+    Button openSignIn, openSignUp, signIn, signUp;
     ImageView closeSignIn, closeSignUp;
-    TextView moveSignIn, moveSignUp;
+    TextView moveSignIn, moveSignUp, invalidName, invalidSurname, invalidEmail,invalidPass, invalidConfirm;
     CardView SignInCard, SignUpCard, googleLogIn, facebookLogIn;
     CheckBox StaySignedIn;
-    EditText logInEmail, logInPass;
+    EditText logInEmail, logInPass, signUpName, signUpSurname, signUpEmail, signUpPass, signUpPassConfirm;
     ActivityResultLauncher<Intent> activityResultLauncher;
     GoogleSignInClient googleSignInClient;
 
@@ -84,6 +77,16 @@ public class MainActivity extends AppCompatActivity {
         StaySignedIn = findViewById(R.id.cbxStaySignedIn);
         logInEmail = findViewById(R.id.edtSignInEmail);
         logInPass = findViewById(R.id.edtSignInPass);
+        signUpName = findViewById(R.id.edtSignUpName);
+        signUpSurname = findViewById(R.id.edtSignUpSurname);
+        signUpEmail = findViewById(R.id.edtSignUpEmail);
+        signUpPass = findViewById(R.id.edtSignUpPass);
+        signUpPassConfirm = findViewById(R.id.edtSignUpConfirmPass);
+        invalidName = findViewById(R.id.lblInvalidName);
+        invalidSurname = findViewById(R.id.lblInvalidSurname);
+        invalidEmail = findViewById(R.id.lblInvalidEmail);
+        invalidPass = findViewById(R.id.lblInvalidPass);
+        invalidConfirm = findViewById(R.id.lblInvalidConfirm);
 
         StaySignedIn.setChecked(prefs.getBoolean("StaySignedIn", false));
         activityResultLauncher = registerForActivityResult(
@@ -185,6 +188,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = logInEmail.getText().toString();
+                String pass = logInPass.getText().toString();
+                if(validateEmail(email) && validatePass(pass, null)){
+                    // add database stuff here
+                }
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = signUpName.getText().toString();
+                String surname = signUpSurname.getText().toString();
+                String email = signUpEmail.getText().toString();
+                String pass = signUpPass.getText().toString();
+                String confirmPass = signUpPassConfirm.getText().toString();
+
+                if(validateName(name) && validateName(surname) && validateEmail(email) && validatePass(pass, null) && validatePass(pass, confirmPass)){
+                    // add database stuff here
+                }
+            }
+        });
+
         googleLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -199,7 +228,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        logInEmail.addTextChangedListener(new TextWatcher() {
+        signUpName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -207,73 +236,175 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+                if(!validateName(charSequence.toString())){
+                    signUpName.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 40), PorterDuff.Mode.SRC_ATOP);
+                    invalidName.getLayoutParams().height = 30;
+                }
+                else{
+                    signUpName.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
+                    invalidName.getLayoutParams().height = 1;
+                }
+                invalidName.requestLayout();
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(!validateEmail(editable.toString())){
-                    logInEmail.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 50), PorterDuff.Mode.SRC_ATOP);
-                }
-                else{
-                    logInEmail.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
-                }
+
             }
         });
 
-        logInPass.addTextChangedListener(new TextWatcher() {
+        signUpSurname.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+                if(!validateName(charSequence.toString())){
+                    signUpSurname.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 40), PorterDuff.Mode.SRC_ATOP);
+                    invalidSurname.getLayoutParams().height = 30;
+                }
+                else{
+                    signUpSurname.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
+                    invalidSurname.getLayoutParams().height = 1;
+                }
+                invalidName.requestLayout();
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(!validatePass(editable.toString())){
-                    logInPass.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 50), PorterDuff.Mode.SRC_ATOP);
-                }
-                else{
-                    logInPass.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
-                }
+
             }
         });
+
+        signUpEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!validateEmail(charSequence.toString())){
+                    signUpEmail.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 40), PorterDuff.Mode.SRC_ATOP);
+                    invalidEmail.getLayoutParams().height = 30;
+                }
+                else{
+                    signUpEmail.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
+                    invalidEmail.getLayoutParams().height = 1;
+                }
+                invalidName.requestLayout();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        signUpPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!validatePass(charSequence.toString(), null)){
+                    signUpPass.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 40), PorterDuff.Mode.SRC_ATOP);
+                    invalidPass.getLayoutParams().height = 30;
+                }
+                else{
+                    signUpPass.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
+                    invalidPass.getLayoutParams().height = 1;
+                }
+                invalidName.requestLayout();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        signUpPassConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!validatePass(charSequence.toString(), signUpPass.getText().toString())){
+                    signUpPassConfirm.getBackground().mutate().setColorFilter(Color.rgb(225, 80, 40), PorterDuff.Mode.SRC_ATOP);
+                    invalidConfirm.getLayoutParams().height = 30;
+                }
+                else{
+                    signUpPassConfirm.getBackground().mutate().setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
+                    invalidConfirm.getLayoutParams().height = 1;
+                }
+                invalidName.requestLayout();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    static public boolean validateName(String name){
+        Pattern special = Pattern.compile("[!@#$%^&*()_+=|<>?{}\\[\\]~/`-]");
+        Matcher isSpecial = special.matcher(name);
+        if(name.isEmpty() || isSpecial.find()){
+            return false;
+        }
+        for (int i = 0; i < name.length(); i++){
+            char c = name.charAt(i);
+            if(Character.isDigit(c)){
+                return false;
+            }
+        }
+        return true;
     }
 
     static public boolean validateEmail(String email){
-        if(!email.contains("@") || !email.contains(".")){
+        if(email.isEmpty() || !email.contains("@") || !email.contains(".")){
             return false;
         }
         return true;
     }
 
-    static public boolean validatePass(String pass){
+    static public boolean validatePass(String pass, String confirm){
         boolean capitalFlag = false, numFlag = false, lowerFlag = false;
-        if(pass.length() < 8){
-            return false;
+        if(confirm == null){
+            if(pass.length() < 8){
+                return false;
+            }
+            for (int i = 0; i < pass.length(); i++){
+                char c = pass.charAt(i);
+                if(Character.isUpperCase(c)){
+                    capitalFlag = true;
+                }
+                else if(Character.isDigit(c)){
+                    numFlag = true;
+                }
+                else if(Character.isLowerCase(c)){
+                    lowerFlag = true;
+                }
+            }
+            if(!capitalFlag || !lowerFlag || !numFlag){
+                return false;
+            }
         }
-        for (int i = 0; i < pass.length(); i++){
-            char c = pass.charAt(i);
-            if(Character.isUpperCase(c)){
-                capitalFlag = true;
-            }
-            else if(Character.isDigit(c)){
-                numFlag = true;
-            }
-            else if(Character.isLowerCase(c)){
-                lowerFlag = true;
-            }
-        }
-        if(!capitalFlag || !lowerFlag || !numFlag){
+        else if(!confirm.equals(pass)){
             return false;
         }
         return true;
     }
 
-    void doGoogleLogIn(){
+    public void doGoogleLogIn(){
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleSignInClient = GoogleSignIn.getClient(this, gso);
         googleSignInClient.signOut();
@@ -287,13 +418,27 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             GoogleSignInAccount account = task.getResult(ApiException.class);
-            Toast.makeText(this, "logged in", Toast.LENGTH_LONG).show();
+            String password = prefs.getString("pass", "N/A");
+            String name = account.getGivenName();
+            String surname = account.getFamilyName();
+            String email = account.getEmail();
+
+            if(password.equals("N/A")){ //if user not in database, make up a pass for database
+                //put database stuff here
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("pass", password);//store password locally for next time
+                editor.commit();
+            }
+            else{ //else sign in
+                //put database stuff here
+            }
+
+            Toast.makeText(this, "Welcome "+name+" "+surname, Toast.LENGTH_LONG).show();
 
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult: failed code= " + e.getStatusCode() + e.getStatus().toString());
-            Toast.makeText(this, "failed", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Unexpected Error", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -316,6 +461,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
