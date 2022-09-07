62fb97b1c0015195198ef2c146987f33f0a1d783
coker-svg <111632162+coker-svg@users.noreply.github.com>
2022-09-07 11:02:00 +0200

    Update HomeActivity.java

--- app/src/main/java/com/coconutbmp/leash/HomeActivity.java
+++ app/src/main/java/com/coconutbmp/leash/HomeActivity.java
@@ -13,6 +13,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
+    //declaring variables
     CardView home_return_button, btnAdd;
     TextView day, month;
 
@@ -20,22 +21,23 @@ public class HomeActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        
+        //initialising variables
         String userID = getIntent().getStringExtra("userID");
-
         btnAdd = findViewById(R.id.btnAddSomething);
         home_return_button = findViewById(R.id.homeReturnCard);
         day = findViewById(R.id.lblDay);
         month = findViewById(R.id.lblMonth);
 
         UXFunctions.setDate(day, month);
-
+        //implementing button to add budget dialogue
         btnAdd.setOnClickListener(view -> {
             AddBudgetDialogue dialogue = new AddBudgetDialogue(this, userID);
             dialogue.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
             dialogue.show();
         });
-
+       
+        //implementing return button
         home_return_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
