f874bd9ad02d53ed98328323050cce01c3ccf973
i-renaissant <hewitt.tny@gmail.com>
2022-09-17 17:02:40 +0200

    navigate to transaction page

---
+++ app/src/main/java/com/coconutbmp/leash/AddTransactionActivity.java
@@ -0,0 +1,14 @@
+package com.coconutbmp.leash;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
