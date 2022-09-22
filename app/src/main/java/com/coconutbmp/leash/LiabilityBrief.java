36b5553015e6f73f61ae17748a39274c11a22758
i-renaissant <hewitt.tny@gmail.com>
2022-09-22 13:13:37 +0200

    - graph plotting start

--- app/src/main/java/com/coconutbmp/leash/LiabilityBrief.java
+++ app/src/main/java/com/coconutbmp/leash/LiabilityBrief.java
@@ -6,12 +6,20 @@ import android.widget.TextView;
 
 
 import com.coconutbmp.leash.BudgetComponents.Liability;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LiabilityBrief extends BudgetComponentFragment {
 
     Liability liability;
+    LineChart lc;
 
     @Override
     void initiate_view() throws Exception{
@@ -24,6 +32,23 @@ public class LiabilityBrief extends BudgetComponentFragment {
         TextView tv = new TextView(this.getContext());
         tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         tv.setText("Paying: R" + (String) json_rep.get("payment_amount") + ", " + (String) json_rep.get("payment_frequency"));
+
+        lc = new LineChart(getContext());
+        LineData ld = new LineData();
+
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        for(int i = 0; i < 1000; i++){
+            entries.add(new Entry(i, (float)Math.sin((double) i/100)));
+        }
+        LineDataSet lds = new LineDataSet(entries, "sine vals");
+
+        ld.addDataSet(lds);
+        lc.setData(ld);
+
+        lc.setMinimumHeight(500);
+
+        details_ll.addView(lc);
         this.details_ll.addView(tv);
 
     }
