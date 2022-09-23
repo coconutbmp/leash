10d3727dcef3de32f5a3cb11a66c41ac1aa85ab0
Liam Carstens <lordinfernus09@gmail.com>
2022-09-23 13:16:19 +0200

    UI Spinner Fixes

--- app/src/main/java/com/coconutbmp/leash/AddLoanDetailsFragment.java
+++ app/src/main/java/com/coconutbmp/leash/AddLoanDetailsFragment.java
@@ -4,6 +4,7 @@ import android.graphics.PorterDuff;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -66,12 +67,28 @@ public class AddLoanDetailsFragment extends Fragment implements LiabilityDetails
         end_date_label = view.findViewById(R.id.loan_end_label);
         repayment_amount_edit = view.findViewById(R.id.loan_repayment_edit);
         payment_freq_spinner = view.findViewById(R.id.loan_payment_freq_spinner);
-
         start_button = view.findViewById(R.id.start_button);
         end_button = view.findViewById(R.id.end_button);
-
         start_dp = view.findViewById(R.id.loan_start_dp);
         end_dp = view.findViewById(R.id.loan_end_dp);
+
+        String[] type = getResources().getStringArray(R.array.interest_types);
+        String[] freq = getResources().getStringArray(R.array.calculation_frequencies);
+
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_text, type);
+        ArrayAdapter<String> freqAdapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_text, freq);
+
+        interest_type_spinner.setPopupBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.color.smokey_white));
+        calculation_freq_spinner.setPopupBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.color.smokey_white));
+        payment_freq_spinner.setPopupBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.color.smokey_white));
+
+        interest_type_spinner.getBackground().setColorFilter(ContextCompat.getColor(getContext(), R.color.black), PorterDuff.Mode.SRC_ATOP);
+        calculation_freq_spinner.getBackground().setColorFilter(ContextCompat.getColor(getContext(), R.color.black), PorterDuff.Mode.SRC_ATOP);
+        payment_freq_spinner.getBackground().setColorFilter(ContextCompat.getColor(getContext(), R.color.black), PorterDuff.Mode.SRC_ATOP);
+
+        interest_type_spinner.setAdapter(typeAdapter);
+        calculation_freq_spinner.setAdapter(freqAdapter);
+        payment_freq_spinner.setAdapter(freqAdapter);
 
         start_button.setOnClickListener(view1 -> UXFunctions.select_date(start_button, start_dp, begin_date_label)); // link the button to the date picker and label
         end_button.setOnClickListener(view1 -> UXFunctions.select_date(end_button, end_dp, end_date_label)); // link the button to the date picker and label
--- app/src/main/java/com/coconutbmp/leash/AddRecurringPaymentFragment.java
+++ app/src/main/java/com/coconutbmp/leash/AddRecurringPaymentFragment.java
@@ -3,6 +3,7 @@ package com.coconutbmp.leash;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -50,12 +51,16 @@ public class AddRecurringPaymentFragment extends Fragment implements LiabilityDe
         payment_freq_spinner = view.findViewById(R.id.payment_freq_spinner);
         begin_date_edit = view.findViewById(R.id.rp_start_label);
         end_date_edit = view.findViewById(R.id.rp_end_label);
-
         start_button = view.findViewById(R.id.set_rp_start_date_button);
         end_button = view.findViewById(R.id.set_rp_end_date_button);
-
         start_dp = view.findViewById(R.id.start_dp);
         end_dp = view.findViewById(R.id.end_dp);
+
+        String[] freq = getResources().getStringArray(R.array.calculation_frequencies);
+        ArrayAdapter<String> freqAdapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_text, freq);
+        payment_freq_spinner.setPopupBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.color.smokey_white));
+        payment_freq_spinner.getBackground().setColorFilter(ContextCompat.getColor(getContext(), R.color.black), PorterDuff.Mode.SRC_ATOP);
+        payment_freq_spinner.setAdapter(freqAdapter);
 
         start_button.setOnClickListener(v1 -> UXFunctions.select_date(start_button, start_dp, begin_date_edit));// link the button to the date picker and label
         end_button.setOnClickListener(v2 -> UXFunctions.select_date(end_button, end_dp, end_date_edit));// link the button to the date picker and label
