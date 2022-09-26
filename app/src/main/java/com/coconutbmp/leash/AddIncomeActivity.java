c032e10a519cdf19a78350914b2ad477a24dd69a
Liam Carstens <lordinfernus09@gmail.com>
2022-09-26 19:10:07 +0200

    Comments

--- app/src/main/java/com/coconutbmp/leash/AddIncomeActivity.java
+++ app/src/main/java/com/coconutbmp/leash/AddIncomeActivity.java
@@ -24,10 +24,7 @@ import java.util.Locale;
  */
 public class AddIncomeActivity extends AppCompatActivity {
 
-    //todo: link elements from activity_add_income.xml to this class
-    //todo: Upload data from the user to the database
-    //todo: navigate back to Budget Page
-
+    //Declare variables
     Button start, end, save;
     DatePicker startDate, endDate;
     TextView lblStartDate, lblEndDate;
@@ -42,6 +39,7 @@ public class AddIncomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_income);
 
+        //Hook xml components to java components
         start = findViewById(R.id.start_button);
         end = findViewById(R.id.end_button);
         save = findViewById(R.id.btnSaveIncome);
@@ -54,41 +52,51 @@ public class AddIncomeActivity extends AppCompatActivity {
         returnCard = findViewById(R.id.ReturnCard);
         incomeFrequency = findViewById(R.id.sprRecurringFrequency2);
 
+        // Call UX Function to save the date from the datepicker to TextView
         start.setOnClickListener(view1 -> UXFunctions.select_date(start, startDate, lblStartDate)); // link the button to the date picker and label
         end.setOnClickListener(view1 -> UXFunctions.select_date(end, endDate, lblEndDate)); // link the button to the date picker and label
 
-        String[] freq = getResources().getStringArray(R.array.calculation_frequencies);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_text, freq);
-        incomeFrequency.setPopupBackgroundDrawable(getResources().getDrawable(R.color.smokey_white));
-        incomeFrequency.getBackground().setColorFilter(getResources().getColor(R.color.black), PorterDuff.Mode.SRC_ATOP);
-        incomeFrequency.setAdapter(adapter);
+        /**
+         * Setup for spinner
+         * Assigns array, layout, colors and text color
+         */
+        String[] freq = getResources().getStringArray(R.array.calculation_frequencies);// Create array
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_text, freq);// Create Spinner adapter
+        incomeFrequency.setPopupBackgroundDrawable(getResources().getDrawable(R.color.smokey_white));// Set Background Color for popup menu
+        incomeFrequency.getBackground().setColorFilter(getResources().getColor(R.color.black), PorterDuff.Mode.SRC_ATOP); //set text color
+        incomeFrequency.setAdapter(adapter); // apply adapter to spinner
 
         save.setOnClickListener(view1 ->{
             addIncome();
         });
 
         returnCard.setOnClickListener(view1->{
-            AddIncomeActivity.this.finish();
+            AddIncomeActivity.this.finish(); // Close Activity
         });
     }
 
+    /**
+     * Void Method
+     * Makes a request to the server to add parameters to Database's Income Table
+     * Closes Activity
+     */
     private void addIncome(){
-        String startDate, endDate, name, frequency, amount;
-        internetRequest = new InternetRequest();
+        String startDate, endDate, name, frequency, amount;// declare variables for parameters
+        internetRequest = new InternetRequest();// create new request
 
-        if (label.getText().toString().equals("") || label.getText().equals(null)){
+        if (label.getText().toString().equals("") || label.getText().equals(null)){// No Name Error
             Toast.makeText(this, "No Name", Toast.LENGTH_SHORT).show();
             return;
         }
-        else{
+        else{ // assign inputted name to variable
             name = label.getText().toString();
         }
 
-        if(lblStartDate.getText().toString().equals("0000/00/00")){
+        if(lblStartDate.getText().toString().equals("0000/00/00")){ // No date selected
             Toast.makeText(this, "Selected a Start Date", Toast.LENGTH_SHORT).show();
             return;
         }
-        else{
+        else{ // assign selected date
             startDate = lblStartDate.getText().toString();
         }
 
@@ -100,24 +108,26 @@ public class AddIncomeActivity extends AppCompatActivity {
             endDate = lblStartDate.getText().toString();
         }
 
-        if(incomeFrequency.getSelectedItemPosition() == 0){
+        if(incomeFrequency.getSelectedItemPosition() == 0){ // no selection
             Toast.makeText(this, "No Selected Income Frequency", Toast.LENGTH_SHORT).show();
             return;
         }
-        else{
+        else{ // get Frequency from spinner
             frequency = incomeFrequency.getSelectedItem().toString().toLowerCase(Locale.ROOT);
         }
 
-        if (incomeAmount.getText().toString().equals("") || incomeAmount.getText().equals(null)){
+        if (incomeAmount.getText().toString().equals("") || incomeAmount.getText().equals(null)){ // no amount inputted
             Toast.makeText(this, "No Amount Received", Toast.LENGTH_SHORT).show();
             return;
         }
-        else{
+        else{ // assign amount
             amount = incomeAmount.getText().toString();
         }
 
+        // create JsonObject for parameters
         JSONObject income = new JSONObject();
 
+        //add values to parameters
         try {
             income.put("incomeName", name);
             income.put("amount", amount);
@@ -125,12 +135,16 @@ public class AddIncomeActivity extends AppCompatActivity {
             income.put("endDate", endDate);
             income.put("freq", frequency);
             income.put("budgetid", Data.current.getJsonRep().get("budget_ID"));
-            internetRequest.doRequest(url + "submit_income.php", this, income, this::processIncome);
+            internetRequest.doRequest(url + "submit_income.php", this, income, this::processIncome); //make request
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Handle success or failure
+     * @param response
+     */
     void processIncome(String response){
         if(response.equals("Success")){
             Toast.makeText(this, "Income Successfully Added", Toast.LENGTH_SHORT).show();
