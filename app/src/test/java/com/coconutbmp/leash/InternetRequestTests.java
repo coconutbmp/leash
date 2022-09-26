71bf4e68dc923367b93c16b0e21a7176f22ca411
Liam Carstens <lordinfernus09@gmail.com>
2022-09-26 13:37:57 +0200

    InternetRequest Test Test

--- app/src/test/java/com/coconutbmp/leash/InternetRequestTests.java
+++ app/src/test/java/com/coconutbmp/leash/InternetRequestTests.java
@@ -1,29 +1,44 @@
 package com.coconutbmp.leash;
 
+import static org.junit.Assert.assertEquals;
+
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 
 import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
 public class InternetRequestTests {
-    MockWebServer mockWebServer;
+    private MockWebServer server;
+
     @Before
-    public void setUp() throws IOException{
-        mockWebServer = new MockWebServer();
-        MockResponse mockSuccessGet = new MockResponse();
-        mockSuccessGet.setResponseCode(HttpURLConnection.HTTP_OK);
-        HttpUrl url = mockWebServer.url("/html/");
-        mockWebServer.start();
+    public void setUp() throws IOException {
+        this.server = new MockWebServer();
+        this.server.start();
     }
 
     @After
-    public void shutDown() throws IOException {
-        mockWebServer.shutdown();
+    public void tearDown() throws IOException {
+        this.server.shutdown();
+    }
+
+    @Test
+    public void Test_SUCCESS() throws Exception {
+        String json = "Success";
+        HttpUrl baseUrl = this.server.url("v1/test/");
+        this.server.enqueue(new MockResponse().setResponseCode(200).setBody(json));
+        JSONObject params = new JSONObject();
+        Request request = new Request.Builder().url(baseUrl).build();
+        OkHttpClient client = new OkHttpClient();
+        String r = client.newCall(request).execute().body().string();
+        assertEquals("Success", r);
     }
 }
