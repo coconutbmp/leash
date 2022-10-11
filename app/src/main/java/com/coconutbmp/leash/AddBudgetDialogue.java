918a45581caecf0ed04427fa9839c90cb752f5ea
Liam Carstens <lordinfernus09@gmail.com>
2022-10-11 14:38:30 +0200

    Fixes

--- app/src/main/java/com/coconutbmp/leash/AddBudgetDialogue.java
+++ app/src/main/java/com/coconutbmp/leash/AddBudgetDialogue.java
@@ -2,9 +2,12 @@ package com.coconutbmp.leash;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
